1564a5f5eec2224197bd4410fb4decb8382d077e
L <eleaz.rs@gmail.com>
2022-12-07 10:59:04 +0100

    initial commit

---
+++ src/main/java/com/ixxus/etram/EtramApplication.java
@@ -0,0 +1,13 @@
+package com.ixxus.etram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtramApplication.class, args);
+	}
+
+}
